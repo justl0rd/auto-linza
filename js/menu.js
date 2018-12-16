c6abd9f9421c4462340f011b8e2eb7fab5fe68b1
Justl0rd <marmorela@ukr.net>
2018-12-16 18:42:07 +0200

    mediaQ

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){ 
+	var touch 	= $('#touch-menu');
+	var menu 	= $('.nav');
+
+	$(touch).on('click', function(e) {
+		e.preventDefault();
+		menu.slideToggle();
+	});
+	
+	$(window).resize(function(){
+		var w = $(window).width();
+		if(w > 991 && menu.is(':hidden')) {
+			menu.removeAttr('style');
+		}
+	});
+	
+});
