f0e285a244134929c3a2b57ec3d91454a46dd06d
Justl0rd <marmorela@ukr.net>
2018-12-24 19:15:49 +0200

    remake header

--- js/preloader.js
+++
@@ -1,7 +0,0 @@
-$( function () {
-	// Preloader
-    var $preloader = $('#page-preloader'),
-        $spinner   = $preloader.find('.spinner');
-    $spinner.fadeOut();
-    $preloader.delay(750).fadeOut('slow');
-});
